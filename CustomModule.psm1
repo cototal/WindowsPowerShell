1922da950bad37f07b97caf5ad66b093399d4d68
Shaun T Martin <martins@surgery.wisc.edu>
2017-03-02 13:40:01 -0600

    Settings don't need to be wrapped in a function.

--- CustomModule.psm1
+++ CustomModule.psm1
@@ -1,3 +1,10 @@
-function Set-CustomProfile() {
-    New-Item alias:grep -value Select-String
-}
+<#
+Set up your profile with:
+
+    New-Item -path $profile -type file -force
+
+Erase that new .ps1 file and clone this repo into the directory created
+#>
+Set-Alias grep Select-String
+Clear-Host
+Write-Host "Profile Loaded"
--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,3 @@
 $modulePath = Split-Path -Path $profile
 $moduleName = "CustomModule.psm1"
 Import-Module "$($modulePath)\$($moduleName)"
-Set-CustomProfile
-Clear-Host
