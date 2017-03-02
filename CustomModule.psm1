138ff98a0f834aefc6121ef255c7a23d189f17e3
Shaun T Martin <martins@surgery.wisc.edu>
2017-03-02 13:48:25 -0600

    Actually useful function.

--- CustomModule.psm1
+++ CustomModule.psm1
@@ -5,6 +5,15 @@ Set up your profile with:
 
 Erase that new .ps1 file and clone this repo into the directory created
 #>
-Set-Alias grep Select-String
+
+# Find files that contain the searchString text. Excluding node_modules for now,
+# may add more directories to exclude later.
+function Select-ItemsInDirectory($searchString) {
+    Get-ChildItem -Recurse *.* |
+    Where-Object { !($_.FullName).Contains("node_modules") } |
+    Select-String -Pattern $searchString |
+    Select-Object -Unique Path
+}
+Set-Alias ack Select-ItemsInDirectory
 Clear-Host
 Write-Host "Profile Loaded"
